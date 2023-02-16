43555db776ba0445670e013ea511ccf2b497d35b
SeymaDemir <seymademir35@gmail.com>
2023-02-16 22:28:48 +0300

    fixed db.json file problem

--- src/app/components/toyscardpage/toyscardpage.component.ts
+++ src/app/components/toyscardpage/toyscardpage.component.ts
@@ -1,18 +1,26 @@
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import toysInfo from '/home/seymademir/Development/angular/minions/db.json';
 
+// interface Toy {
+//   id: Number;
+//   name: String;
+//   desciption: String;
+//   price: Number;
+//   imgSrc: String;
+// }
 
 @Component({
   selector: 'app-toyscardpage',
   templateUrl: './toyscardpage.component.html',
-  styleUrls: ['./toyscardpage.component.css']
+  styleUrls: ['./toyscardpage.component.css'],
 })
-export class ToyscardpageComponent implements OnInit{
-  public toysInfo: any;
-  public constructor(private http: HttpClient){}
-  public ngOnInit(): void {
-    const url: string = 'http://localhost:3000/minions/';
-    this.http.get(url).subscribe((response) => this.toysInfo = response)
-  }
- 
+export class ToyscardpageComponent {
+  toys= toysInfo;
+
+  //  public constructor() {}
+  //  public ngOnInit(): void {
+  //   const url: string = 'http://localhost:3000/minions/';
+  //   this.http.get(url).subscribe((response) => this.toysInfo = response)
+  // }
 }
